aab27daa5588d9c45a5a4d8f65e52d173ca0a9c2
Lauri Tahvanainen <48516187+LauriTahvanainen@users.noreply.github.com>
2019-03-20 21:17:27 +0200

    Viikko 2 laskarit, lopullinen commit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,50 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void lataaminenToimii() {
+        kortti.lataaRahaa(1000);
+        kortti.lataaRahaa(5);
+        assertEquals("saldo: 10.15", kortti.toString());
+    }
+    
+//    @Test
+//    public void negatiivinenLataaminenEiOnnistu() {
+//        kortti.lataaRahaa(-10);
+//        kortti.lataaRahaa(-5);
+//        assertEquals("saldo: 0.10", kortti.toString());
+//    }
+    
+    @Test
+    public void otaRahaaToimiiJosRahaaOn() {
+        kortti.lataaRahaa(100);
+        kortti.otaRahaa(50);
+        assertEquals("saldo: 0.60", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaSaldoEiMuutuJosRahaaEiTarpeeksi() {
+        kortti.otaRahaa(50);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrueJosRahaaTarpeeksi() {
+        kortti.lataaRahaa(150);
+        boolean tulos = kortti.otaRahaa(70);
+        assertTrue(tulos);
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalseJosRahaaEiTarpeeksi() {
+        kortti.lataaRahaa(150);
+        boolean tulos = kortti.otaRahaa(200);
+        assertFalse(tulos);
+    }
 }
