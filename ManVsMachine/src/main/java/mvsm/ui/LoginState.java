ed8fb5b9734091121bb623baebf54b92900fee7a
Lauri Tahvanainen <48516187+LauriTahvanainen@users.noreply.github.com>
2019-04-24 15:53:42 +0300

    Update loginState ui. Add password fields. Make it possible to add the machine start anywhere in the map.

--- ManVsMachine/src/main/java/mvsm/ui/LoginState.java
+++ ManVsMachine/src/main/java/mvsm/ui/LoginState.java
@@ -10,30 +10,34 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 public final class LoginState extends State {
 
-    private Pane currentPane;
-    private final HBox root;
-    private final VBox signInPane;
+    private final BorderPane root;
+    private final GridPane signInPane;
     private final VBox createAccountPane;
     private final StateManager gsm;
     private final UserDao userDao;
+    private Text errorText1;
+    private Text errorText2;
+    private TextField username;
+    private TextField newUsername;
 
     public LoginState(StateManager gsm, UserDao userDao) {
         this.gsm = gsm;
-        this.root = new HBox();
+        this.root = new BorderPane();
         this.root.setPrefSize(this.gsm.getScene().getWidth(), this.gsm.getScene().getHeight());
-        this.signInPane = new VBox();
-        this.signInPane.setPrefSize(this.gsm.getScene().getWidth(), this.gsm.getScene().getHeight());
+        this.signInPane = new GridPane();
+        this.root.setCenter(this.signInPane);
         this.createAccountPane = new VBox();
-        this.createAccountPane.setPrefSize(this.gsm.getScene().getWidth(), this.gsm.getScene().getHeight());
-        this.currentPane = this.root;
         this.userDao = userDao;
         initPane();
     }
@@ -45,51 +49,57 @@ public final class LoginState extends State {
 
     @Override
     public void initPane() {
-        //first view
-        root.setAlignment(Pos.CENTER);
-        Button quit = new Button("Quit");
-        Button signIn = new Button("Sign in with an existing account");
-        Button createNewAccount = new Button("Create a new account");
-        root.getChildren().addAll(signIn, createNewAccount, quit);
-
         //sign in view
-        signInPane.setAlignment(Pos.CENTER);
-        Button signIn2 = new Button("Sign in");
-        TextField username = new TextField();
-        username.setMaxWidth(200);
-        Button returnB1 = new Button("Return");
-        Text text = new Text("");
-        this.signInPane.getChildren().addAll(username, signIn2, returnB1, text);
+        this.signInPane.setAlignment(Pos.CENTER);
+        Button quit = new Button("Quit");
+        Button signIn = new Button("Sign in");
+        Button createNewAccount = new Button("Create new account");
+        createNewAccount.setPrefWidth(210);
+        createNewAccount.setTextAlignment(TextAlignment.CENTER);
+        Text usernameText = new Text("Username");
+        Text passwordText = new Text("Password");
+        this.errorText1 = new Text();
+        this.username = new TextField();
+        PasswordField password = new PasswordField();
+        username.setMaxWidth(210);
+        username.setPromptText("Username");
+        password.setMaxWidth(210);
+        password.setPromptText("Password");
+        this.signInPane.add(errorText1, 1, 0);
+        this.signInPane.addRow(1, usernameText, username);
+        this.signInPane.addRow(2, passwordText, password);
+        this.signInPane.addRow(3, signIn, createNewAccount, quit);
+        
 
         //create account view
         createAccountPane.setAlignment(Pos.CENTER);
-        Button returnB2 = new Button("Return");
-        TextField newUsername = new TextField();
-        newUsername.setMaxWidth(200);
+        Button returnB = new Button("Return");
+        returnB.setPrefWidth(210);
+        returnB.setTextAlignment(TextAlignment.CENTER);
+        this.newUsername = new TextField();
+        PasswordField newPassword1 = new PasswordField();
+        PasswordField newPassword2 = new PasswordField();
+        newUsername.setMaxWidth(210);
+        newUsername.setPromptText("New username");
+        newPassword1.setMaxWidth(210);
+        newPassword1.setPromptText("New password");
+        newPassword2.setMaxWidth(210);
+        newPassword2.setPromptText("Confirm new password");
         Button createAccount = new Button("Create account");
-        Text text2 = new Text();
-        this.createAccountPane.getChildren().addAll(newUsername, createAccount, returnB2, text2);
+        createAccount.setPrefWidth(210);
+        createAccount.setTextAlignment(TextAlignment.CENTER);
+        this.errorText2 = new Text();
+        this.createAccountPane.getChildren().addAll(newUsername, newPassword1, newPassword2, createAccount, returnB, errorText2);
     }
 
     @Override
     public void handleAction(ActionEvent t) {
-        if (this.currentPane.equals(this.signInPane)) {
-            handleSignInView(t, (Text) this.signInPane.getChildren().get(3), (TextField) this.signInPane.getChildren().get(0));
+        if (this.root.getCenter().equals(this.signInPane)) {
+            handleSignInView(t);
             return;
         }
-        if (this.currentPane.equals(this.createAccountPane)) {
-            handleCreateAccountView(t, (Text) this.createAccountPane.getChildren().get(3), (TextField) this.createAccountPane.getChildren().get(0));
-            return;
-        }
-        Button button = (Button) t.getTarget();
-        if (button.getText().equals("Sign in with an existing account")) {
-            this.currentPane = this.signInPane;
-            this.gsm.setSceneRoot(this.signInPane);
-        } else if (button.getText().equals("Create a new account")) {
-            this.currentPane = this.createAccountPane;
-            this.gsm.setSceneRoot(this.createAccountPane);
-        } else {
-            Platform.exit();
+        if (this.root.getCenter().equals(this.createAccountPane)) {
+            handleCreateAccountView(t);
         }
     }
 
@@ -100,55 +110,55 @@ public final class LoginState extends State {
 
     @Override
     public Pane getCurrent() {
-        return this.currentPane;
+        return this.root;
     }
 
-    private void handleSignInView(ActionEvent t, Text errorText, TextField inputField) {
+    private void handleSignInView(ActionEvent t) {
         Button button = (Button) t.getTarget();
-        if (button.getText().equals("Sign in")) {
-            User user = null;
+        String text = button.getText();
+        if (text.equals("Sign in")) {
+            User user;
             try {
-                user = this.userDao.read(inputField.getText());
+                user = this.userDao.read(this.username.getText());
                 if (user != null) {
-                    inputField.clear();
-                    errorText.setText("");
+                    this.username.clear();
+                    this.errorText1.setText("");
                     this.gsm.setCurrentUser(user);
                     this.gsm.setCurrentState(1);
                     this.gsm.setSceneRoot(this.gsm.getCurrentState().getCurrent());
                     this.gsm.getCurrentState().restore();
-                    this.currentPane = this.root;
                 } else {
-                    inputField.clear();
-                    errorText.setText("The username doesn't exist!");
+                    this.username.clear();
+                    this.errorText1.setText("The username doesn't exist!");
                 }
             } catch (SQLException ex) {
-                errorText.setText("There was an unexpected error!");
+                errorText1.setText("There was an unexpected error!");
             }
 
+        } else if (text.equals("Create new account")) {
+            errorText1.setText("");
+            username.clear();
+            this.root.setCenter(this.createAccountPane);
         } else {
-            errorText.setText("");
-            inputField.clear();
-            this.currentPane = this.root;
-            this.gsm.setSceneRoot(this.currentPane);
+            Platform.exit();
         }
     }
 
-    private void handleCreateAccountView(ActionEvent t, Text errorText, TextField inputField) {
+    private void handleCreateAccountView(ActionEvent t) {
         Button button = (Button) t.getTarget();
         if (button.getText().equals("Create account")) {
             try {
-                int ret = this.userDao.create(inputField.getText());
-                reactToUsernameCreation(ret, errorText, inputField.getText());
-                inputField.clear();
+                int ret = this.userDao.create(this.newUsername.getText());
+                reactToUsernameCreation(ret, this.newUsername.getText());
+                newUsername.clear();
             } catch (SQLException ex) {
-                errorText.setText("Unexpected error!");
-                inputField.clear();
+                errorText2.setText("Unexpected error!");
+                newUsername.clear();
             }
         } else {
-            errorText.setText("");
-            inputField.clear();
-            this.currentPane = this.root;
-            this.gsm.setSceneRoot(this.currentPane);
+            errorText2.setText("");
+            newUsername.clear();
+            this.root.setCenter(this.signInPane);
         }
 
     }
@@ -161,17 +171,17 @@ public final class LoginState extends State {
     public void restore(Algorithm a, String map) {
     }
 
-    private void reactToUsernameCreation(int ret, Text errorText, String username) {
+    private void reactToUsernameCreation(int ret, String username) {
         if (ret == 1) {
-            errorText.setText("Username: '" + username + "' added!");
+            errorText2.setText("Username: '" + username + "' added!");
         } else if (ret == 0) {
-            errorText.setText("The username is already taken!");
+            errorText2.setText("The username is already taken!");
         } else if (ret == 3) {
-            errorText.setText("Given username contains a space!");
+            errorText2.setText("Given username contains a space!");
         } else if (ret == 4) {
-            errorText.setText("Given username too short!");
+            errorText2.setText("Given username too short!");
         } else {
-            errorText.setText("Given username too long!");
+            errorText2.setText("Given username too long!");
         }
     }
 
